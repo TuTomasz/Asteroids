29c41cbdb549cef656636c3769e4f0d61ba5f9fe
Tomasz Turek <ttomaszito@gmail.com>
2020-05-10 19:56:01 -0400

    initialized scaffold for states

--- src/main.js
+++ src/main.js
@@ -1,45 +1,19 @@
+import menuState from "./states/menu";
+import gameState from "./states/game";
+
+// Initialize canvas
 const canvas = document.getElementById("asteroids");
-/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
-// Presets
-const frameRate = 30;
+// Initialize game menu state
+menuState(canvas, ctx);
 
-/**
- * check player keybord input
- */
-checkinput = () => {
-  document.addEventListener("keydown", (event) => {
-    if (event.keyCode == 38) {
-      console.log("up");
-    } else if (event.keyCode == 40) {
-      console.log("down");
-    } else if (event.keyCode == 37) {
-      console.log("left");
-    } else if (event.keyCode == 39) {
-      console.log("right");
-    } else if (event.keyCode == 32) {
-      console.log("fire");
-    } else {
-      console.log("other input");
-    }
-  });
+let start = (event) => {
+  document.removeEventListener("keydown", start);
+
+  // initialize game state
+  gameState(canvas, ctx);
 };
 
-/**
- * main game loop
- */
-update = () => {
-  // draw space
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
-  //draw ship
-  ctx.strokeStyle = "white";
-  ctx.strokeRect(75, 140, 150, 110);
-
-  checkinput();
-};
-
-// game loop frame rate
-setInterval(update(), 1000 / frameRate);
+// initialize menu event listeneers
+document.addEventListener("keydown", start);
