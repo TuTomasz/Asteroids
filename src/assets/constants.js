26c59003818935ed5c80b644360b8d41b6d85bf0
Tomasz Turek <ttomaszito@gmail.com>
2020-05-16 20:40:21 -0400

    created a constants object for easy game setting manipulation and optimization

---
+++ src/assets/constants.js
@@ -0,0 +1,21 @@
+module.exports = {
+  //Asteroid Constants
+  ASTEROID_NUM: 3, //starting asteroid number
+  ASTEROID_SPEED: 50, // asteroid speed
+  ASTEROID_SIZE: 100, // size in pixels
+  ASTEROID_EDGES: 10, // arbitrary number of edges
+  ASTEROID_DISTORTION: 0.5,
+
+  // Ship Constants
+  SHIP_SIZE: 20,
+  ACCELERATION: 3,
+  FRICTION: 2,
+  TURN_RATE: 360,
+
+  // Game Constants
+  FRAME_RATE: 60,
+
+  // Dev Flags
+  SHIP_BOUNDING_BOX: true,
+  ASTERIOD_BOUNDING_BOX: true,
+};
